2826e63138aac18f8e2b59954090a5d0967818e9
Anh <59912241+AnhQuan94@users.noreply.github.com>
2021-01-14 17:03:29 +0100

    add esayadmin bundle

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class,[
+                'disabled' => true,
+                'label'=> 'Email'
+            ])
+            ->add('firstname', TextType::class,[
+                'disabled' => true,
+                'label'=> 'Prénom'
+            ])
+            ->add('lastname', TextType::class,[
+                'disabled' => true,
+                'label'=> 'Nom'
+            ])
+            ->add('old_password', PasswordType::class,[
+                'label'=> 'Mot de passe actuel',
+                'mapped'=>false,
+                'attr'=>[
+                    'placeholder' => 'Veuillez entrer votre mot de passe actuel'
+                ]
+            ])
+            ->add('new_password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'mapped'=> false,
+                'invalid_message' => ' Veuillez entrer un mot de passe identique',
+                'label' => 'Nouveau mot de passe',
+                'required' => true,
+                'first_options' => [ 
+                    'label' => 'Nouveau mot de passe',
+                    'attr' =>  [
+                        'placeholder' => 'Nouveau mot de passe'
+                    ]
+                    
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez le nouveau mot de passe',
+                    'attr' =>  [
+                        'placeholder' => 'Cormfirmez le nouveau mot de passe'
+                    ]
+                    ]
+                
+            ])
+
+            ->add('submit', SubmitType::class,[
+                'label' => 'Changer de mot de passe'
+            ])
+
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
